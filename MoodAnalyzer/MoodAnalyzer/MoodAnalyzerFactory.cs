018db6cb40b6560771883de2625f830267b5376e
unknown <hindhujhasridhar@gmail.com>
2022-01-23 22:56:59 +0530

    PARAMETER CONSTRUCTOR IS CREATED

--- MoodAnalyzer/MoodAnalyzer/MoodAnalyzerFactory.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -6,6 +6,7 @@ namespace MoodAnalyzer
 {
     public class MoodAnalyzerFactory
     {
+
         public static object CreateMoodAnalyse(string className,string constructorName)
         {
             string pattern = @"." + constructorName + "$";
@@ -26,6 +27,29 @@ namespace MoodAnalyzer
             else
             {
                 throw new CustomException(CustomException.Exception_Type.NO_SUCH_METHOD, "CONSTRUCTOR IS NOT FOUND");
+            }
+        }
+
+        public static object CreateMoodAnalyseWithoutParameter(string className,string constructorName)
+        {
+            Type type = typeof(Program);
+            if(type.Name.Equals(className)||type.FullName.Equals(className))
+            {
+                if(type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    string instance = ctor.Invoke(new object[] { "HAPPY" });
+                    return instance;
+                }
+                else
+                {
+                    throw new CustomException(CustomException.Exception_Type.NO_SUCH_METHOD, "CONSTRUCTOR IS NOT FOUND");
+                        .
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.Exception_Type.NO_SUCH_CLASS, "CLASS NOT FOUND");
             }
         }
     }
