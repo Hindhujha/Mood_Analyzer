b6d4b3b99beccec0e152279ead9a4c09ee061200
unknown <hindhujhasridhar@gmail.com>
2022-01-23 11:36:54 +0530

    MOOD ANALYZER PROGRAMS

---
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAMS");
+        }
+    }
+}
