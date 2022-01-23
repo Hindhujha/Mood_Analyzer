bfae20b8869705d188934f53390d7a3a5a7ec62d
unknown <hindhujhasridhar@gmail.com>
2022-01-23 22:08:25 +0530

    OBJECT CREATED FOR REFLECTION

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzer
+{
+    internal class MoodAnalyzerFactory
+    {
+        public static object CreateMoodAnalyse(string className,string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if(result.Success)
+            {
+                try
+                {
+                    Assembly executing=Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType=executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch(ArgumentNullException)
+                {
+                    throw new CustomException(CustomException.Exception_Type.NO_SUCH_CLASS, "CLASS NOT FOUND");
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.Exception_Type.NO_SUCH_METHOD, "CONSTRUCTOR IS NOT FOUND");
+            }
+        }
+    }
+}
