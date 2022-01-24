b317bb4f53e25a828a65b34a7e316e9de7ddea12
unknown <hindhujhasridhar@gmail.com>
2022-01-24 11:34:20 +0530

    INVOKED IN REFLECTION

--- MoodAnalyzer/MoodAnalyzer/MoodAnalyzerFactory.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -63,6 +63,22 @@ public static object CreateMoodAnalyse(string className,string constructorName)
             }
         }
 
+        public static string InvokeAnalyseMood(string message,string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyzer.Program");
+                object moodAnalyseObject = MoodAnalyzerFactory.CreateMoodAnalyseParameter("MoodAnalyzer.Program", "Program", message);
+                MethodInfo analyseMoodInfo=type.GetMethod(methodName);
+                object mood = analyseMoodInfo.Invoke(moodAnalyseObject, null);
+                return mood.ToString();
+            }
+            catch(NullReferenceException)
+            {
+                throw new CustomException(CustomException.Exception_Type.NO_SUCH_METHOD, "METHOD IS NOT FOUND");
+            }
+        }
+
      
     }
 }
